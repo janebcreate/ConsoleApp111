3f443e743872e882c8cab5bff9dc8d8cf8abacd4
eugeniabud <evgenia.zrain@mail.ru>
2022-04-20 23:38:47 +0300

    Добавьте файлы проекта.

---
+++ ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp1
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;" +
+            "Port=5432;" +
+            "Database=testdb;" +
+            "Username=postgres;" +
+            "Password=1488");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using ConsoleApp1;
+
+namespace HelloApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // добавление данных
+            ApplicationContext db = new ApplicationContext();
+
+            // создаем два объекта User
+            User user1 = new User { Id = 1, Name = "Tom", Age = 33 };
+            User user2 = new User { Id = 2, Name = "Alice", Age = 26 };
+            // добавляем их в БД
+            db.Users.AddRange(user1, user2);
+            db.SaveChanges(); // не работает сохранение
+
+            // получаем объекты из БД и выводим на консоль
+            var users = db.Users.ToList();
+            Console.WriteLine("Users list:");
+            foreach (User u in users)
+            {
+                Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+            }
+        }
+    }
+}
